d5e2d34d28cc9952f936dcc59c2611109df99913
syedhazeera <129369062+syedhazeera@users.noreply.github.com>
2023-04-17 18:29:36 +0530

    Add files via upload

---
+++ close().c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <fcntl.h>
+int main()
+{
+    int fd1 = open("foo.txt", O_RDONLY);
+    if (fd1 < 0)
+    {
+        perror("c1");
+        exit(1);
+    }
+    printf("opened the fd = % d\n", fd1);
+     
+    // Using close system Call
+    if (close(fd1) < 0)
+    {
+        perror("c1");
+        exit(1);
+    }
+    printf("closed the fd.\n");
+}
+
+/*output:
+syed@syed-VirtualBox:~/Desktop/linux$ ./close
+opened the fd =  3
+closed the fd.*/
---
+++ open().c
@@ -0,0 +1,28 @@
+// open system call
+#include<stdio.h>
+#include<fcntl.h>
+#include<errno.h>
+extern int errno;
+int main()
+{    
+    // if file does not have in directory
+    // then file foo.txt is created.
+    int fd = open("foo.txt", O_RDONLY | O_CREAT);
+     
+    printf("fd = %d\n", fd);
+     
+    if (fd ==-1)
+    {
+        // print which type of error have in a code
+        printf("Error Number % d\n", errno);
+         
+        // print program detail "Success or failure"
+        perror("Program");                
+    }
+    return 0;
+}
+
+/*output:
+syed@syed-VirtualBox:~/Desktop/linux$ gcc open.c -o open
+syed@syed-VirtualBox:~/Desktop/linux$ ./open
+fd = 3*/
